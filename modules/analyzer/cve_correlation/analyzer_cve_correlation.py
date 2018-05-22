bea1a9acde921c4585d449d66fd6b75ab2c307a1
Dustin Born <dustin.born@gmx.de>
2018-05-23 01:36:52 +0200

    Add endscore & CVSSv2 conversion to CVE correlation module
    Add computation of final end cvssv3 score for every host.
    
    Add conversion of CVSSv2 into CVSSv3 score for CVEs that do not
    have an associated CVSSv3 score on nvd.nist.gov .

--- modules/analyzer/cve_correlation/analyzer_cve_correlation.py
+++ modules/analyzer/cve_correlation/analyzer_cve_correlation.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import copy
+from cvsslib import cvss3, calculate_vector
 import json
 import os
 import requests
@@ -18,6 +19,8 @@ LOGFILE = ""
 
 logger = None
 
+CVE_AMOUNT = 0
+
 def conduct_analysis(results: list):
     """
     Analyze the specified hosts in HOSTS for CVEs belonging regarding its software.
@@ -26,7 +29,9 @@ def conduct_analysis(results: list):
     """
     
     def process_port_cves(protocol):
+        global CVE_AMOUNT
         nonlocal hosts
+
         for _, portinfo in host[protocol].items():
             if "cpes" in portinfo:
                 service_cpes = portinfo["cpes"]
@@ -34,6 +39,7 @@ def conduct_analysis(results: list):
                 for cpe in service_cpes:
                     # get cpe cves
                     cves = get_cves(vulners_api, cpe)
+                    CVE_AMOUNT += len(cves)
                     cves_dict = {}
                     for cve in cves:
                         cves_dict[cve["id"]] = cve
@@ -42,15 +48,16 @@ def conduct_analysis(results: list):
                 # TODO: implement
                 pass
 
+    global CVE_AMOUNT, logger   
     # setup logger
     logger = util.get_logger(__name__, LOGFILE)
     logger.info("Starting with CVE analysis")
 
     cve_results = {}
     vulners_api = vulners.Vulners()
-    # hosts = copy.deepcopy(HOSTS)
-    hosts = {'192.168.0.101': {'os': {'name': 'Apple OS X 10.10.X', 'cpes': ['cpe:/o:apple:mac_os_x:10.10'], 'accuracy': '97', 'type': 'general purpose'}, 'ip': {'addr': '192.168.0.101', 'type': 'ipv4'}, 'mac': {'addr': '', 'vendor': ''}, 'tcp': {'80': {'portid': '80', 'protocol': 'tcp', 'state': 'open', 'name': 'http', 'product': 'Apache httpd', 'version': '2.4.29', 'extrainfo': '(Unix)', 'cpes': ['cpe:/a:apache:http_server:2.4.29']}}, 'udp': {}}}
+    hosts = copy.deepcopy(HOSTS)
 
+    logger.info("Starting with CVE discovery of all hosts")
     for _, host in hosts.items():
         # get OS CVEs
         if "cpes" in host["os"]:
@@ -59,6 +66,7 @@ def conduct_analysis(results: list):
             for cpe in os_cpes:
                 # get cpe cves
                 cves = get_cves(vulners_api, cpe)
+                CVE_AMOUNT += len(cves)
                 cves_dict = {}
                 for cve in cves:
                     cves_dict[cve["id"]] = cve
@@ -74,17 +82,65 @@ def conduct_analysis(results: list):
     with open(HOST_CVE_FILE, "w") as f:
         f.write(json.dumps(hosts, ensure_ascii=False, indent=3))
 
-    scores = determine_final_scores(hosts)
+    logger.info("Done")
+    logger.info("Computing final CVSSv3 scores for all hosts")
+    scores = calculate_final_scores(hosts)
+    logger.info("Done")
     created_files = [HOST_CVE_FILE]
-    results.append(({}, created_files))
-    # results.append((hosts, created_files))
+    results.append((scores, created_files))
 
 
-def determine_final_scores(hosts: dict):
-    # TODO: implement
-    # TODO: aggregate using weights to have high scores be more impactful
-    for _, host in hosts.items():
-        pass
+def calculate_final_scores(hosts: dict):
+    def process_port_cve_scores(protocol):
+        global CVE_AMOUNT
+        nonlocal unnormalized_score_sum, weight_sum
+        
+        for _, portinfo in host[protocol].items():
+            if "cpes" in portinfo:
+                service_cpes = portinfo["cpes"]
+                for cpe in service_cpes:
+                    for _, cve in portinfo["cpes"][cpe].items():
+                        cvssv3_score = float(cve["cvssv3"])
+                        weight = (1/CVE_AMOUNT) * cvssv3_score**2 * (cvssv3_score/10)
+                        weight_sum += weight
+                        unnormalized_score_sum += weight * cvssv3_score
+            else:
+                # TODO: implement
+                pass
+
+    host_scores = {}
+    for ip, host in hosts.items():
+        unnormalized_score_sum = 0
+        weight_sum = 0
+
+        # get OS CVEs
+        if "cpes" in host["os"]:
+            os_cpes = host["os"]["cpes"]
+            for cpe in os_cpes:
+                for _, cve in os_cpes[cpe].items():
+                    cvssv3_score = float(cve["cvssv3"])
+                    weight = (1/CVE_AMOUNT) * cvssv3_score * (cvssv3_score/10)
+                    weight_sum += weight
+                    unnormalized_score_sum += weight * cvssv3_score
+        else:
+            # TODO: implement
+            pass
+
+        # get TCP and UDP cvssv3 score
+        process_port_cve_scores("tcp")
+        process_port_cve_scores("udp")
+
+        if weight_sum:  # check if weight_sum is not zero
+            end_score = unnormalized_score_sum/weight_sum
+            end_score = max(0, end_score)  # ensure score is greater than 0
+            end_score = min(10, end_score)  # ensure score is less than 10
+            end_score = str(end_score)  # turn into str (to have an alternative if no score exists, i.e. N/A)
+        else:
+            end_score = "N/A"
+
+        host_scores[ip] = end_score
+
+    return host_scores
 
 
 def slim_cve_results(cve_results: list):
@@ -98,11 +154,16 @@ def slim_cve_results(cve_results: list):
         slimmed_results.append(slimmed_result)
     return slimmed_results
 
+
 def get_cves(vulners_api, cpe: str):
     with warnings.catch_warnings():  # ignore warnings that vulners might throw
         warnings.simplefilter("ignore")
-        cve_results = vulners_api.cpeVulnerabilities(cpe, maxVulnerabilities=500)  # TODO: handle limit better?
-    
+        try:
+            cve_results = vulners_api.cpeVulnerabilities(cpe, maxVulnerabilities=500)  # TODO: handle limit better?
+        except ValueError:
+            logger.warning("Getting CVEs for CPE '%s' resulted in a ValueError. Maybe CPE is malformed.")
+            cve_results = {}
+
     cve_results = cve_results.get("NVD", [])
     if cve_results:
         cve_results = slim_cve_results(cve_results)
@@ -110,6 +171,7 @@ def get_cves(vulners_api, cpe: str):
             cve_id = cve_result["id"]
             add_additional_cve_info(cve_result)
     return cve_results
+
 
 def add_additional_cve_info(cve: dict):
     # TODO: check if HTTP Response is OK before using retrieved HTML
@@ -122,21 +184,114 @@ def add_additional_cve_info(cve: dict):
     cvssv3_score_tag = soup.find("span", {"data-testid" : "vuln-cvssv3-base-score"})
     if cvssv3_score_tag:
         cve["cvssv3"] = cvssv3_score_tag.text.strip()
+    else:
+        cvssv2_score_tag = soup.find("span", {"data-testid" : "vuln-cvssv2-base-score"})
+        if cvssv2_score_tag:
+            cve["cvssv2"] = cvssv2_score_tag.text.strip()
 
     # retrieve the short form of the attack vector
-    vector_short_tag = soup.find("span", {"data-testid" : "vuln-cvssv3-vector"})
+    if "cvssv3" in cve:
+        vector_short_tag = soup.find("span", {"data-testid" : "vuln-cvssv3-vector"})
+    elif "cvssv2" in cve:
+        vector_short_tag = soup.find("span", {"data-testid" : "vuln-cvssv2-vector"})
+    else:
+        vector_short_tag = None
+
     if vector_short_tag:
         cve["vector_short"] = vector_short_tag.text.strip().split(" ")[0].strip()  # split at space to ignore the (V3 legend) at the end
 
     # retrieve the full text version of the attack vector
     cve["vector_detail"] = {}
-    vector_detail_container = soup.find("p", {"data-testid" : "vuln-cvssv3-metrics-container"})
+    if "cvssv3" in cve:
+        vector_detail_container = soup.find("p", {"data-testid" : "vuln-cvssv3-metrics-container"})
+    elif "cvssv2" in cve:
+        vector_detail_container = soup.find("p", {"data-testid" : "vuln-cvssv2-metrics-container"})
+    else:
+        vector_detail_container = None
+
     if vector_detail_container:
         strong_tags = vector_detail_container.findAll("strong")
         span_tags = vector_detail_container.findAll("span")
-
         for i, strong_tag in enumerate(strong_tags):
             span_tag = span_tags[i]
+            for br in span_tag.find_all("br"):
+                br.replace_with("\n")
             attr = strong_tag.text.strip()[:-1]  # ignore last text character (a colon)
             value = span_tag.text.strip()
-            cve["vector_detail"][attr] = value
+            cve["vector_detail"][attr] = value  # replace possible HTML <br> tags with newline character
+
+    if "cvssv2" in cve:
+        transfrom_cvssv2_to_cvssv3(cve)
+
+
+def transfrom_cvssv2_to_cvssv3(cve: dict):
+    # Conversion incentives are takten from: https://security.stackexchange.com/questions/127335/how-to-convert-risk-scores-cvssv1-cvssv2-cvssv3-owasp-risk-severity
+    # If the conversion incentive is indecisive, the more likely conversion was taken
+    converted_cvssv3_vector = ""
+    vector_fields = cve["vector_short"][1:-1].split("/")  # remove left and right parenthesis
+    for vector_field in vector_fields:
+        key, val = vector_field.split(":")
+        if key == "AV":
+            converted_cvssv3_vector += "%s:%s/" % (key, val)  # just copy AV field
+        elif key == "AC":
+            if val == "M":
+                val = "H"
+            converted_cvssv3_vector += "%s:%s/" % (key, val)
+        elif key == "Au":
+            if val == "S":
+                val = "L"
+            elif val == "M":
+                val = "H"
+            converted_cvssv3_vector += "%s:%s/" % ("PR", val)
+        elif key == "Au":
+            if val == "S":
+                val = "L"
+            elif val == "M":
+                val = "H"
+            converted_cvssv3_vector += "%s:%s/" % (key, val)
+        elif key == "C" or key == "I" or key == "A":
+            if val == "C":
+                val = "H"
+            elif val == "P":
+                val = "L"
+            elif val == "N":
+                val = "N"
+            converted_cvssv3_vector += "%s:%s/" % (key, val)
+        elif key == "RL":
+            if val == "OF":
+                val = "O"
+            elif val == "TF":
+                val = "T"
+            elif val == "ND":
+                val = "X"
+            converted_cvssv3_vector += "%s:%s/" % (key, val)
+        elif key == "RL":
+            if val == "UR":
+                val = "R"
+            elif val == "UC":
+                val = "U"
+            elif val == "ND":
+                val = "X"
+            converted_cvssv3_vector += "%s:%s/" % (key, val)
+
+    if "C:C" in converted_cvssv3_vector and "I:C" in converted_cvssv3_vector and "A:C" in converted_cvssv3_vector:
+        converted_cvssv3_vector += "S:C/"
+    else:
+        converted_cvssv3_vector += "S:U/"
+
+    if "AC:H" in cve["vector_short"]:
+        converted_cvssv3_vector += "UI:R/"
+    else:
+        converted_cvssv3_vector += "UI:N/"
+
+    converted_cvssv3_vector = converted_cvssv3_vector[:-1]  # remove trailing /
+
+    del cve["vector_detail"]
+    cve["orig_cvssv2"] = cve["cvssv2"]
+    del cve["cvssv2"]
+    cve["orig_cvssv2_vector"] = cve["vector_short"]
+    del cve["vector_short"]
+    cve["converted_cvssv3_vector"] = "(%s)" % converted_cvssv3_vector
+    vector_v3 = "CVSS:3.0/" + converted_cvssv3_vector
+    cvssv3 = str(calculate_vector(vector_v3, cvss3)[0])  # get base score of cvssv3 score vector
+    cve["cvssv3"] = cvssv3
