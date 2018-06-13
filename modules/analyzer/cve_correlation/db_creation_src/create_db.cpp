ad08a1190ae7579ca4ccd02b346015f663b32ba7
Dustin Born <dustin.born@gmx.de>
2018-06-14 01:26:39 +0200

    Improve parsing of CVE data feed
    Now the parser recognizes ANDs and ORs in the "configurations" field
    and acts accordingly by storing info about the cpe combination in
    the DB.
    
    Note: the C++ code is very unclear and barely commented. This needs
    to change in the future, when the final requirements are known.

--- modules/analyzer/cve_correlation/db_creation_src/create_db.cpp
+++ modules/analyzer/cve_correlation/db_creation_src/create_db.cpp
@@ -7,6 +7,7 @@
 #include <string>
 #include <cstdlib>
 #include <climits>
+#include <unordered_set>
 
 extern "C" {
     #include "dirent.h"
@@ -20,7 +21,31 @@ struct VagueCpeInfo {
     std::string version_start_type;
     std::string version_end;
     std::string version_end_type;
+
+    bool operator==(const VagueCpeInfo &other) const {
+        return vague_cpe == other.vague_cpe &&
+                version_start == other.version_start &&
+                version_start_type == other.version_start_type &&
+                version_end == other.version_end &&
+                version_end_type == other.version_end_type;
+    }
 };
+
+namespace std {
+    template<>
+    struct hash<VagueCpeInfo> {
+        std::size_t operator()(const VagueCpeInfo &vi) const {
+            using std::size_t;
+            using std::hash;
+            using std::string;
+            return (hash<string>()(vi.vague_cpe)
+                     ^ hash<string>()(vi.version_start)
+                     ^ hash<string>()(vi.version_start_type)
+                     ^ hash<string>()(vi.version_end)
+                     ^ hash<string>()(vi.version_end_type));
+        }
+    };
+}
 
 void handle_exception(SQLite::Exception &e) {
     std::string msg = e.what();
@@ -29,11 +54,40 @@ void handle_exception(SQLite::Exception &e) {
     }
 }
 
+void get_specific_cpes(json &vendor_data, std::string &vague_cpe, std::unordered_set<VagueCpeInfo> &specific_cpes) {
+    std::string cpe_part_str, cpe_str;
+    for (auto &vendor_data_entry : vendor_data) {
+        auto &vendor_name_ref = vendor_data_entry["vendor_name"];  // field must exist afaik
+        if (vendor_data_entry.find("product") != vendor_data_entry.end()) {
+            if (vendor_data_entry["product"].find("product_data") != vendor_data_entry["product"].end()) {
+                for (auto &product_data_entry : vendor_data_entry["product"]["product_data"]) {
+                    auto &product_name_ref = product_data_entry["product_name"];  // field must exist afaik
+                    cpe_part_str = vendor_name_ref.get<std::string>() + ":" + product_name_ref.get<std::string>();
+                    if (vague_cpe.find(cpe_part_str) != std::string::npos) {
+                        if (product_data_entry.find("version") != product_data_entry.end()) {
+                            if (product_data_entry["version"].find("version_data") != product_data_entry["version"].end()) {
+                                for (auto &version_data_entry : product_data_entry["version"]["version_data"]) {
+                                    if (version_data_entry.find("version_value") != version_data_entry.end()) {
+                                        auto &version_value_ref = version_data_entry["version_value"];
+                                        VagueCpeInfo vcpe = {vague_cpe + ":" + version_value_ref.get<std::string>(), "", "", "", ""};
+                                        specific_cpes.emplace(vcpe);
+                                    }
+                                }
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+
 int add_to_db(SQLite::Database &db, const std::string &filepath) {
     // Begin transaction
     SQLite::Transaction transaction(db);
     SQLite::Statement cve_query(db, "INSERT INTO cve VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-    SQLite::Statement cve_cpe_query(db, "INSERT INTO cve_cpe VALUES (?, ?, ?, ?, ?, ?)");
+    SQLite::Statement cve_cpe_query(db, "INSERT INTO cve_cpe VALUES (?, ?, ?, ?, ?, ?, ?)");
 
     // read a JSON file
     std::ifstream input_file(filepath);
@@ -102,55 +156,140 @@ int add_to_db(SQLite::Database &db, const std::string &filepath) {
         std::vector<VagueCpeInfo> vague_cpe_infos;
 
         for (auto &config_nodes_entry : cve_entry["configurations"]["nodes"]) {
-            for (auto &cpe_entry : config_nodes_entry["cpe"]) {
-                vulnerable = cpe_entry["vulnerable"];
-                if (!vulnerable)
+            if (config_nodes_entry.find("cpe") != config_nodes_entry.end()) {
+                for (auto &cpe_entry : config_nodes_entry["cpe"]) {
+                    vulnerable = cpe_entry["vulnerable"];
+                    if (!vulnerable)
+                        continue;
+                    cpe = cpe_entry["cpe22Uri"];
+                    VagueCpeInfo vague_cpe_info = {cpe, "", "", "", ""};
+
+                    if (cpe_entry.find("versionStartIncluding") != cpe_entry.end()) {
+                        vague_cpe_info.version_start = cpe_entry["versionStartIncluding"];
+                        vague_cpe_info.version_start_type = "Including";
+                    }
+                    else if (cpe_entry.find("versionStartExcluding") != cpe_entry.end()) {
+                        vague_cpe_info.version_start = cpe_entry["versionStartExcluding"];
+                        vague_cpe_info.version_start_type = "Excluding";
+                    }
+
+                    if (cpe_entry.find("versionEndIncluding") != cpe_entry.end()) {
+                        vague_cpe_info.version_end = cpe_entry["versionEndIncluding"];
+                        vague_cpe_info.version_end_type = "Including";
+                    }
+                    else if (cpe_entry.find("versionEndExcluding") != cpe_entry.end()) {
+                        vague_cpe_info.version_end = cpe_entry["versionEndExcluding"];
+                        vague_cpe_info.version_end_type = "Excluding";
+                    }
+
+                    if (vague_cpe_info.version_start != "" || vague_cpe_info.version_end != "") {
+                        vague_cpe_infos.push_back(vague_cpe_info);
+                    }
+
+                    cve_cpe_query.bind(2, cpe);
+                    cve_cpe_query.bind(3, vague_cpe_info.version_start);
+                    cve_cpe_query.bind(4, vague_cpe_info.version_start_type);
+                    cve_cpe_query.bind(5, vague_cpe_info.version_end);
+                    cve_cpe_query.bind(6, vague_cpe_info.version_end_type);
+                    cve_cpe_query.bind(7, "");
+
+                    try {
+                        cve_cpe_query.exec();
+                    }
+                    catch (SQLite::Exception& e) {
+                        handle_exception(e);
+                    }
+
+                    try {
+                        cve_cpe_query.reset();
+                    }
+                    catch (SQLite::Exception& e) {
+                        handle_exception(e);
+                    }
+                }
+            }
+            else if (config_nodes_entry.find("children") != config_nodes_entry.end()) {
+                if (config_nodes_entry.find("operator") == config_nodes_entry.end()) {
+                    std::cerr << "Cannot parse CVE " << cve_id << " properly. Needs fixing!" << std::endl;
                     continue;
-
-                cpe = cpe_entry["cpe22Uri"];
-
-                VagueCpeInfo vague_cpe_info = {cpe, "", "", "", ""};
-
-                if (cpe_entry.find("versionStartIncluding") != cpe_entry.end()) {
-                    vague_cpe_info.version_start = cpe_entry["versionStartIncluding"];
-                    vague_cpe_info.version_start_type = "Including";
                 }
-                else if (cpe_entry.find("versionStartExcluding") != cpe_entry.end()) {
-                    vague_cpe_info.version_start = cpe_entry["versionStartExcluding"];
-                    vague_cpe_info.version_start_type = "Excluding";
+                else if (config_nodes_entry["operator"] != "AND") {
+                    std::cerr << "Cannot parse CVE " << cve_id << " properly. Needs fixing!" << std::endl;
+                    continue;
                 }
 
-                if (cpe_entry.find("versionEndIncluding") != cpe_entry.end()) {
-                    vague_cpe_info.version_end = cpe_entry["versionEndIncluding"];
-                    vague_cpe_info.version_end_type = "Including";
-                }
-                else if (cpe_entry.find("versionEndExcluding") != cpe_entry.end()) {
-                    vague_cpe_info.version_end = cpe_entry["versionEndExcluding"];
-                    vague_cpe_info.version_end_type = "Excluding";
-                }
+                std::vector<std::unordered_set<VagueCpeInfo> > and_cpes;
 
-                if (vague_cpe_info.version_start != "" || vague_cpe_info.version_end != "") {
-                    vague_cpe_infos.push_back(vague_cpe_info);
-                }
+                for (auto &children_entry : config_nodes_entry["children"]) {
+                    std::unordered_set<VagueCpeInfo> cpes;
+                    for (auto &cpe_entry : children_entry["cpe"]) {
+                        vulnerable = cpe_entry["vulnerable"];
+                        if (!vulnerable)
+                            continue;
+                        cpe = cpe_entry["cpe22Uri"];
 
-                cve_cpe_query.bind(2, cpe);
-                cve_cpe_query.bind(3, vague_cpe_info.version_start);
-                cve_cpe_query.bind(4, vague_cpe_info.version_start_type);
-                cve_cpe_query.bind(5, vague_cpe_info.version_end);
-                cve_cpe_query.bind(6, vague_cpe_info.version_end_type);
+                        VagueCpeInfo vague_cpe_info = {cpe, "", "", "", ""};
 
-                try {
-                    cve_cpe_query.exec();
-                }
-                catch (SQLite::Exception& e) {
-                    handle_exception(e);
-                }
+                        if (cpe_entry.find("versionStartIncluding") != cpe_entry.end()) {
+                            vague_cpe_info.version_start = cpe_entry["versionStartIncluding"];
+                            vague_cpe_info.version_start_type = "Including";
+                        }
+                        else if (cpe_entry.find("versionStartExcluding") != cpe_entry.end()) {
+                            vague_cpe_info.version_start = cpe_entry["versionStartExcluding"];
+                            vague_cpe_info.version_start_type = "Excluding";
+                        }
 
-                try {
-                    cve_cpe_query.reset();
+                        if (cpe_entry.find("versionEndIncluding") != cpe_entry.end()) {
+                            vague_cpe_info.version_end = cpe_entry["versionEndIncluding"];
+                            vague_cpe_info.version_end_type = "Including";
+                        }
+                        else if (cpe_entry.find("versionEndExcluding") != cpe_entry.end()) {
+                            vague_cpe_info.version_end = cpe_entry["versionEndExcluding"];
+                            vague_cpe_info.version_end_type = "Excluding";
+                        }
+
+                        if (vague_cpe_info.version_start != "" || vague_cpe_info.version_end != "") {
+                            get_specific_cpes(cve_entry["cve"]["affects"]["vendor"]["vendor_data"], cpe, cpes);
+                        }
+
+                        cpes.emplace(vague_cpe_info);
+
+                    }
+                    and_cpes.emplace_back(cpes);
                 }
-                catch (SQLite::Exception& e) {
-                    handle_exception(e);
+                for (int i = 0; i < and_cpes.size(); i++) {
+                    for (auto &cpe_outer : and_cpes[i]) {
+                        std::string and_str = "";
+                        for (int j = 0; j < and_cpes.size(); j++) {
+                            if (i == j)
+                                continue;
+                            for (auto &cpe_inner : and_cpes[j]) {
+                                and_str += cpe_inner.vague_cpe + ",";
+                            }
+                        }
+                        if (and_str != "")
+                           and_str.pop_back();
+
+                        cve_cpe_query.bind(2, cpe_outer.vague_cpe);
+                        cve_cpe_query.bind(3, cpe_outer.version_start);
+                        cve_cpe_query.bind(4, cpe_outer.version_start_type);
+                        cve_cpe_query.bind(5, cpe_outer.version_end);
+                        cve_cpe_query.bind(6, cpe_outer.version_end_type);
+                        cve_cpe_query.bind(7, and_str);
+
+                        try {
+                            cve_cpe_query.exec();
+                        }
+                        catch (SQLite::Exception& e) {
+                            handle_exception(e);
+                        }
+
+                        try {
+                            cve_cpe_query.reset();
+                        }
+                        catch (SQLite::Exception& e) {
+                        }
+                    }
                 }
             }
         }
@@ -201,7 +340,7 @@ int add_to_db(SQLite::Database &db, const std::string &filepath) {
                             }
                         }
                     }
-                }  
+                }
             }
         }
     }
@@ -239,8 +378,8 @@ int main(int argc, char *argv[]) {
         db.exec("CREATE TABLE cve (cve_id VARCHAR(25), description TEXT, published DATETIME, last_modified DATETIME, \
             cvss_version CHAR(3), base_score CHAR(3), vector VARCHAR(60), severity VARCHAR(15), PRIMARY KEY(cve_id))");
         db.exec("CREATE TABLE cve_cpe (cve_id VARCHAR(25), cpe TEXT, cpe_version_start VARCHAR(255), cpe_version_start_type VARCHAR(50), \
-            cpe_version_end VARCHAR(255), cpe_version_end_type VARCHAR(50), PRIMARY KEY(cve_id, cpe, cpe_version_start, \
-            cpe_version_start_type, cpe_version_end, cpe_version_end_type))");
+            cpe_version_end VARCHAR(255), cpe_version_end_type VARCHAR(50), with_cpes TEXT, PRIMARY KEY(cve_id, cpe, cpe_version_start, \
+            cpe_version_start_type, cpe_version_end, cpe_version_end_type, with_cpes))");
 
         DIR *dir;
         struct dirent *ent;
