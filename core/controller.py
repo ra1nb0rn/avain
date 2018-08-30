8f29ec0a9764c5ac1b60b8e10bd7291dcd171068
Dustin Born <dustin.born@gmx.de>
2018-08-30 21:56:19 +0200

    Fix bug

--- core/controller.py
+++ core/controller.py
@@ -234,7 +234,7 @@ class Controller():
         network_scores = {}
         net_dir_map = {}
 
-        if self.single_network or len(networks) == 1:
+        if self.single_network or len(networks) == 1 or self.analysis_only:
             score = do_analysis_helper(networks, self.output_dir)
             if score is not None:
                 network_scores["assessed_network"] = score
