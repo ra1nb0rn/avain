be0607a159a7e48f8d2dff8d1ee7de0d39dbf210
Dustin Born <dustin.born@gmx.de>
2019-09-16 20:50:11 +0200

    Do not use nlohmann's JSON parser as submodule

--- modules/cve_correlation/avain_build.sh
+++ modules/cve_correlation/avain_build.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# configure submodules of SQLiteCpp
 cd "db_creation_src/SQLiteCpp"
 git submodule init
 git submodule update
 cd ".."
+
+# get C++ JSON parser from https://github.com/nlohmann/json
+mkdir -p "json/single_include/nlohmann"
+cd json/single_include/nlohmann
+wget https://raw.githubusercontent.com/nlohmann/json/develop/single_include/nlohmann/json.hpp
+cd "../../../"
 
 rm -rf build
 mkdir -p build
