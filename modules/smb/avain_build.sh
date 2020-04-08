60b790884f57ccf520eea15cba91b2399b533b39
Dustin Born <dustin.born@gmx.de>
2020-04-08 23:44:51 +0200

    Remove unnecessary statements

--- modules/smb/avain_build.sh
+++ modules/smb/avain_build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-QPRINT="--quiet"
-if [ $QUIET != 1 ]; then
-    QPRINT=""
-fi
-
 git clone ${QPRINT} https://github.com/ShawnDEvans/smbmap.git
 cd smbmap
 pip3 install ${QPRINT} -r requirements.txt
