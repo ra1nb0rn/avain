ceb83755747928ed54bcd99fbfc0247ec7f8aeac
Dustin Born <dustin.born@gmx.de>
2020-04-11 19:39:27 +0200

    Add alternative installation of WPScan

--- modules/web/cms_scanner/wpscan/avain_build.sh
+++ modules/web/cms_scanner/wpscan/avain_build.sh
@@ -20,15 +20,33 @@ else
     # only install WPScan if it is not already installed to avoid any conflicts in e.g. Kali
     which wpscan &> /dev/null
     if [ $? != 0 ]; then
+        # first try to install directly via package manager
         if [ ${QUIET} != 1 ]; then
             sudo ${LINUX_PACKAGE_MANAGER} -y install wpscan
         else
             sudo ${LINUX_PACKAGE_MANAGER} -y install wpscan >/dev/null
         fi
         wpscan -h &> /dev/null
+
+        # if install did not work, install Ruby and then wpscan via gem
         if [ $? != 0 ]; then
-            printf "${RED}Could not find or install wpscan.\\nPlease install it manually and then restart the overall installation.\\n"
-            exit 1
+            if [ ${QUIET} != 1 ]; then
+                sudo ${LINUX_PACKAGE_MANAGER} -y install ruby rubygems
+            else
+                sudo ${LINUX_PACKAGE_MANAGER} -y install ruby rubygems >/dev/null
+            fi
+            if [ ${QUIET} != 1 ]; then
+                gem install wpscan
+            else
+                gem install wpscan --quiet --silent
+            fi
+
+            # check if install worked this time
+            wpscan -h &> /dev/null
+            if [ $? != 0 ]; then
+                printf "${RED}Could not find or install wpscan.\\nPlease install it manually and then restart the overall installation.\\n"
+                exit 1
+            fi
         fi
     fi
 fi
